63f1ee5a9b623fa79a156dd76f866fed0d972a5f
anatmb <anatmb@hotmail.com>
2025-01-12 14:47:41 -0300

    Mejorando la estructura de la carpeta backend

---
+++ backend/api/src/app.js
@@ -0,0 +1,11 @@
+const express = require ("express");
+const morgan= require("morgan");
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res)=>{ 
+    res.status(200).send("llegue al endpoint");
+});
+module.exports= app;
